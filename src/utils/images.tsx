42a456c7ed1d2b7791301f2936207bd9eef02a72
chirag <chiragkapadiya1998@gmail.com>
2024-07-11 09:33:11 +0530

    add icon

--- src/utils/images.tsx
+++ src/utils/images.tsx
@@ -1,4 +1,17 @@
 export const Icons = {
   launch_screen: require('../assets/Images/launch_screen.png'),
   onboardingW1: require('../assets/Images/w1.png'),
+  bgLogin: require('../assets/Images/bgLogin.png'),
+
+
+
+  //ICON
+  apple: require('../assets/icons/apple.png'),
+  back: require('../assets/icons/back.png'),
+  eyeIn: require('../assets/icons/eyeIn.png'),
+  eyeOff: require('../assets/icons/eyeOff.png'),
+  facebook: require('../assets/icons/facebook.png'),
+  mapPin: require('../assets/icons/mapPin.png'),
+  eyeIn: require('../assets/icons/eyeIn.png'),
+
 };
